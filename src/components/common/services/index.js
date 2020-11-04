651d6f3056d7c8027a1c89ccf511b3adaf7d662b
jiangxindong <jiangxindong@cestc.cn>
2020-11-04 14:27:33 +0800

    目录结构优化

---
+++ src/components/common/services/index.js
@@ -0,0 +1,3 @@
+export { default as ApiService } from './api.service';
+export { default as ApiUrl } from './api.url';
+export { default as Notify } from './notify';
---
+++ src/components/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ data }}</h1>
+  </div>
+</template>
+
+<script>
+import ApiService from "./common/services";
+
+export default {
+  data() {
+    return {
+      data: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
